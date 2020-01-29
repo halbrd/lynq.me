22cae9e139f9f702c104e173c022310ebbcd22d8
William Holt <halbrd@gmail.com>
2020-01-29 22:18:50 +1100

    delete temporary skyrim page

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
 from flask import Flask, render_template
 
 from views.lemmy import lemmy
-from views.skyrim import skyrim
 from views.bts import bts
 
 app = Flask(__name__)
 
 app.register_blueprint(lemmy)
-app.register_blueprint(skyrim)
 app.register_blueprint(bts)
 
 @app.route('/')
--- views/skyrim.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint, render_template
-
-skyrim = Blueprint('skyrim', __name__)
-
-@skyrim.route('/skyrim/')
-def index():
-    return render_template('skyrim/index.html')
